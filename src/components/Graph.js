c348ce8086edfbb840104ccca3a909ea793bc298
TT-Linux <kyro.eth@gmail.com>
2022-12-07 12:28:34 +0800

    Added graph labels and lower intervals

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,14 +1,25 @@
+import {useParams} from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import 'chart.js/auto';
 import { Line } from 'react-chartjs-2';
 
 function Graph() {
-    const data = {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    
+
+      const [sevenDays,setSevenDays ] = useState([])
+      const [allPrices, setAllPrice ] = useState([])
+      useEffect(() => {
+        fetch('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7&interval=hourly')
+        .then(res=>res.json())
+        .then(
+            (result) => setSevenDays(result["prices"]))
+      },[])
+      const data = {
+        labels:  sevenDays.map(day => new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(day[0])),
         datasets: [
           {
             label: "Coin Price",
-            data: [33, 53, 85, 41, 44, 65],
+            data: sevenDays.map(price => price[1]),
             fill: true,
             backgroundColor: "rgba(75,192,192,0.2)",
             borderColor: "rgba(75,192,192,1)"
