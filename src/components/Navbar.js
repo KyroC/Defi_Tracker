ae521d5b505a4d704a2dcbf61a51653e785d8302
KyroC <Kyroeth@gmail.com>
2023-09-07 19:48:59 +0800

    Updated Navbar to use MUI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,36 @@
-import './Navbar.css';
+import { Drawer, Divider, List, ListItem} from "@mui/material"
 import { Link } from 'react-router-dom';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-function Navbar() {
-  return (
-    <div className="Navbar">
-      <ul> Dashboards
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/wallet">Wallet</Link></li>
-        <li><Link to="/market">Market</Link></li>
-      </ul>
-    </div>
-  );
+let drawerWidth = 240
+
+function Navbar(){
+    return (
+      <Drawer
+        PaperProps= {{
+          sx: {
+            width:drawerWidth
+          }
+        }}
+        variant = "permanent"
+        anchor = "left"
+        >
+        <DeleteIcon />
+       <Divider />
+       <List>
+       <ListItem key="Dashboard">
+          Dashboard
+        </ListItem>
+        <ListItem key="Markets">
+          <Link to={`/Markets`}>
+          Markets
+          </Link>
+        </ListItem>
+        <ListItem key="My Accounts">
+          My Accounts
+        </ListItem>
+       </List>
+      </Drawer>
+    )
 }
-
 export default Navbar;
