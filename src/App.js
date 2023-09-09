858f64b6783ff91ff3148fd577ca7af8176c418e
KyroC <Kyroeth@gmail.com>
2023-09-09 23:54:27 +0800

    Added Dashboard Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Navbar from './components/Navbar.js';
 import Markets from './components/Markets.js';
 import Coin from './components/Coin.js';
+import Dashboard from './components/Dashboard.js'
 import Searchbar from './components/Searchbar.js';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -14,6 +15,7 @@ function App() {
         <BrowserRouter>
           <Navbar />
           <Routes>
+            <Route path="/" element={<Dashboard />} />
             <Route path="/markets" element={<Markets />} />
             <Route path="/coin/:coinName" element={<Coin />} />
           </Routes>
