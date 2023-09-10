b0d39b369d39e2bf09d628194dd333f17e882e59
KyroC <Kyroeth@gmail.com>
2023-09-10 19:16:24 +0800

    Added MUI grid

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,24 +1,36 @@
 import {styled} from '@mui/material/styles'
-import {Box, Grid, Paper } from '@mui/material'
+import {Box, Grid, Paper, Card} from '@mui/material'
 
-const Item = styled(Paper)(({ theme }) => ({
+/*
+const= styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
   }));
-  
+  */
 
 function Dashboard(){
     return (
-        <Box sx={{flexGrow: 1}}>
-            <Grid container spacing={2} direction="row" xs={12} md={4}>
-                <Grid Item>
-                    <Item>Item 1</Item>
+        <Box
+            sx={{ml:30}}>
+            <Grid container spacing={2}
+                    sx={{
+                        width:1
+                    }}
+                    >
+                <Grid item xs={4}>
+                    xs=8
                 </Grid>
-                <Grid Item>
-                    Item 2
+                <Grid item xs={4}>
+                    xs=4
+                </Grid>
+                <Grid item xs={4}>
+                    xs=4
+                </Grid>
+                <Grid item xs={8}>
+                    xs=8
                 </Grid>
             </Grid>
         </Box>
