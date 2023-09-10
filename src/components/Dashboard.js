9b72dda6312b266c7529bd240861c27acdb818af
KyroC <Kyroeth@gmail.com>
2023-09-10 20:02:34 +0800

    added Dashboard layout + data description

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import {styled} from '@mui/material/styles'
-import {Box, Grid, Paper, Card} from '@mui/material'
+import {Box, Grid, Paper, Card, CardActions, CardContent, Typography} from '@mui/material'
 
 /*
 const= styled(Paper)(({ theme }) => ({
@@ -14,23 +14,79 @@ const= styled(Paper)(({ theme }) => ({
 function Dashboard(){
     return (
         <Box
-            sx={{ml:30}}>
-            <Grid container spacing={2}
-                    sx={{
-                        width:1
-                    }}
+            sx={{
+                ml:30,
+                pt:3,
+                px:2,
+            }}>
+            <Typography variant="h4">
+                My Portfolio
+            </Typography>
+            <Grid 
+              container 
+              justifyContent="space-around"
+              spacing={5}
+              sx={{
+                  width:1,
+                  textAlign: 'center',
+                  alignItems:'center',
+              }}
                     >
                 <Grid item xs={4}>
-                    xs=8
+                    <Card >
+                        <CardContent>
+                            Portfolio Value:
+                        </CardContent>
+                    </Card>
                 </Grid>
                 <Grid item xs={4}>
-                    xs=4
+                    <Card>
+                    <CardContent>
+                        Portfolio breakdown based on Coins held (Sort by highest)
+                    </CardContent>
+                    </Card>
                 </Grid>
                 <Grid item xs={4}>
-                    xs=4
+                    <Card sx={{ minWidth: 1 }}>
+                        <CardContent>
+                            Hollow Piechart showing how much value help in each asset
+                        </CardContent>
+                    </Card>
                 </Grid>
-                <Grid item xs={8}>
-                    xs=8
+            </Grid>
+            <Typography variant="h4">
+                BTC Market
+            </Typography>
+            <Grid 
+              container 
+              justifyContent="space-around"
+              spacing={5}
+              sx={{
+                  width:1,
+                  textAlign: 'center',
+                  alignItems:'center',
+              }}
+                    >
+                <Grid item xs={4}>
+                    <Card >
+                        <CardContent>
+                            Overview of BTC market 
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                    <Card>
+                    <CardContent>
+                       Last 24 hrs/Last week/Last month
+                    </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                    <Card sx={{ minWidth: 1 }}>
+                        <CardContent>
+                           Some valuable data(Total shorts, big whale movements)
+                        </CardContent>
+                    </Card>
                 </Grid>
             </Grid>
         </Box>
