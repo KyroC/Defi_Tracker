fe0a92206547e1f83eb76ecfa9151a6b339ab1ed
KyroC <Kyroeth@gmail.com>
2023-09-10 19:15:43 +0800

    Added topbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Markets from './components/Markets.js';
 import Coin from './components/Coin.js';
 import Dashboard from './components/Dashboard.js'
 import Searchbar from './components/Searchbar.js';
+import TopBar from './components/Topbar.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -14,6 +15,7 @@ function App() {
     <div className="App">
         <BrowserRouter>
           <Navbar />
+          <TopBar />
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/markets" element={<Markets />} />
